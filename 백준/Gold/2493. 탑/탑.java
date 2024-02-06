3630dc88fc3be3bb2ff707dbd3193b3e6064d08c
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-06 09:26:40 +0900

    [Gold V] Title: 탑, Time: 568 ms, Memory: 106220 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -2,36 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
-		int n = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
 		
+		int n = Integer.parseInt(st.nextToken());
 		ArrayDeque<int[]> deque = new ArrayDeque<>();
+		
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			while(!deque.isEmpty() && deque.peekLast()[0] < num) {
+			
+			while(!deque.isEmpty() && deque.peekLast()[1] < num) {
 				deque.pollLast();
 			}
-
-			if(deque.isEmpty()) sb.append(0).append(" ");
-			else sb.append(deque.peekLast()[1]).append(" ");
+			if(deque.size() == 0 ) {
+				sb.append(0).append(" ");
+			}else {
+				sb.append(deque.peekLast()[0]).append(" ");
+			}
 			
-			deque.addLast(new int[] {num, i+1});
+			
+			deque.addLast(new int[] {i + 1, num});
 		}
-	
-		
-	
 		System.out.println(sb);
+		
 		
 	}
 }
