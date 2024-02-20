b5c2d953ea9b44c32a0ef60bcc2612a80ef6ee2a
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-21 08:51:25 +0900

    [Gold III] Title: 줄 세우기, Time: 424 ms, Memory: 44144 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,17 +3,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n;
 	static int m;
-	static int[] count;
 	static ArrayList<Integer>[] adjList;
-	
+	static int[] count;
 	static ArrayDeque<Integer> deque;
-	
 	static StringBuilder sb;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,18 +20,15 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
+		adjList = new ArrayList[n+1]; 
 		count = new int[n+1];
-		adjList = new ArrayList[n+1];
 		deque = new ArrayDeque<>();
-		
-		
 		for (int i = 1; i <= n; i++) {
 			adjList[i] = new ArrayList<>();
 		}
 		
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			
@@ -43,32 +37,28 @@ public class Main {
 		}
 		
 		for (int i = 1; i <= n; i++) {
-			if(count[i] == 0) {
-				deque.addLast(i);
-			}
+			if(count[i] == 0) deque.addLast(i);
 		}
 		
-		bfs();
-		
+		sorted();
 		System.out.println(sb);
 	}
-	
-	static void bfs() {
+	static void sorted() {
 		while(!deque.isEmpty()) {
 			int now = deque.pollFirst();
 			
-			if(count[now] == 0) {
-				sb.append(now).append(" ");
-			}
-			
-			int size = adjList[now].size();
-			
-			for (int i = 0; i < size; i++) {
+			sb.append(now).append(" ");
+			for (int i = 0; i < adjList[now].size(); i++) {
 				int next = adjList[now].get(i);
-				count[next] -- ;
 				
-				if(count[next] == 0) deque.addLast(next);
+				count[next]--;
+				
+				if(count[next] == 0) {
+					deque.addLast(next);
+				}
 			}
 		}
 	}
+	
+	
 }
