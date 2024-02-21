5d443d4c693ac40efc2234888d96f3879150f40f
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-21 14:22:04 +0900

    [Gold V] Title: 암호 만들기, Time: 88 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -2,64 +2,77 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int n;
-	public static int m;
+	static StringBuilder sb;
+	static int l;
+	static int c;
+	static HashSet<Character> set;
+	static String[] arr;
+	static boolean[] visited;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		sb = new StringBuilder();
 		
-		m = Integer.parseInt(st.nextToken());
-		n = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 		
-		char[] arr = new char[n];
-		boolean[] visited = new boolean[n];
+		set = new HashSet<>();
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+		
+		arr = new String[c];
+		visited = new boolean[c];
 		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < n; i++) {
-			String s = st.nextToken();
-			arr[i] = s.charAt(0);
+		
+		for (int i = 0; i < c; i++) {
+			arr[i] = st.nextToken();
+		}
+		Arrays.sort(arr);
+		
+		backtrack(0, 0 , new StringBuilder());
+		System.out.println(sb);
+	}
+	static boolean check(String s) {
+		int a = 0;
+		int b = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			if(set.contains(s.charAt(i))) {
+				a ++;
+			}else b++;
 		}
 		
-		Arrays.sort(arr);
-		backtrack(0, 0, arr, visited, new StringBuilder());
+		if(a < 1 || b < 2) return false;
+		else return true;
+		
+		
 		
 	}
 	
-	public static void backtrack(int depth, int index ,char[] arr, boolean[] visited, StringBuilder sb) {
-		if(depth == m) {
-			if(check(sb.toString())) {
-				System.out.println(sb);
+	static void backtrack(int depth, int index, StringBuilder s) {
+		if(depth == l) {
+			if(check(s.toString())) {
+				sb.append(s).append("\n");
 			}
-			
 			return;
 		}
 		
-		for (int i = index; i < n; i++) {
+		for (int i = index; i < c; i++) {
 			if(!visited[i]) {
 				visited[i] = true;
-				sb.append(arr[i]);
-				backtrack(depth + 1, i, arr, visited, sb);
-				sb.deleteCharAt(sb.length()-1);
+				s.append(arr[i]);
+				backtrack(depth + 1, i, s);
+				s.deleteCharAt(s.length() -1);
 				visited[i] = false;
 			}
 		}
-	}
-	
-	public static boolean check(String s) {
-		int consonant = 0;
-		int vowel = 0;
-		
-		
-		for (int i = 0; i < s.length(); i++) {
-			if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') {
-				consonant += 1;
-			}else vowel +=1;
-		}
-		
-		if(consonant >=1 && vowel >=2) return true;
-		else return false;
 	}
 }
