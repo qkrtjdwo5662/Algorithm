f5cbf4db8decf755ee02f016ddfbb18e38326b8c
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-21 15:42:25 +0900

    [Gold V] Title: ABCDE, Time: 208 ms, Memory: 13052 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -6,58 +6,68 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int count = 0;
-    public static boolean[] visited;
-    public static ArrayList<Integer>[] adjList;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        adjList = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adjList[i] = new ArrayList<>();
-        }
-
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            adjList[u].add(v);
-            adjList[v].add(u);
-        }
-
-        visited = new boolean[n]; // 방문체크
-
-//
-        for (int i = 0; i < n; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                dfs(i, 0);
-                visited[i] = false;
-            }
-        }
-        System.out.println(0);
-    }
-
-    public static void dfs(int now, int depth){
-        if(depth == 4) {
-            System.out.println(1);
-            System.exit(0);
-            return;
-        }
-
-
-        for (int i = 0; i < adjList[now].size(); i++) {
-            if(!visited[adjList[now].get(i)]){
-                visited[adjList[now].get(i)] = true;
-                dfs(adjList[now].get(i), depth+1);
-                visited[adjList[now].get(i)] = false;
-            }
-        }
-    }
+	static int n;
+	static int m;
+	static ArrayList<Integer>[] adjList;
+	static boolean[] visited;
+	static boolean flag;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		adjList = new ArrayList[n];
+		
+		flag = false;
+		for (int i = 0; i < n; i++) {
+			adjList[i] = new ArrayList<>();
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			adjList[a].add(b);
+			adjList[b].add(a);
+		}
+		
+		visited = new boolean[n];
+		for (int i = 0; i < n; i++) {
+			visited[i] = true;
+			dfs(0, i);
+			visited[i] = false;
+		}
+		
+		if(flag) {
+			sb.append(1).append("\n");
+		}else sb.append(0).append("\n");
+		System.out.println(sb);
+		
+	}
+	
+	static void dfs(int depth, int now) {
+		if(flag) return;
+		
+		if(depth == 4) {
+			flag = true;
+			return;
+		}
+		
+		for (int i = 0; i < adjList[now].size(); i++) {
+			int next = adjList[now].get(i);
+			if(!visited[next]) {
+				visited[next] = true;
+				dfs(depth + 1, next);
+				visited[next] = false;
+			}
+		}
+	}
+	
+	
 }
