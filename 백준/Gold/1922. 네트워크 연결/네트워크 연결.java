c81c4957cee76d93eeb1832e78a40b4f1ed68041
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-23 11:05:51 +0900

    [Gold IV] Title: 네트워크 연결, Time: 612 ms, Memory: 49976 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,79 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Node{
+	static class Edge{
+		int start;
 		int end;
 		int cost;
 		
-		public Node(int end, int cost) {
+		public Edge(int start, int end, int cost) {
+			this.start = start;
 			this.end = end;
 			this.cost = cost;
 		}
 	}
-	
-	static ArrayList<Node>[] adjList;
-	static boolean[] visited;
-	static int answer;
+	static int[] parents;
+	static Edge[] edgeList;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		
 		int n = Integer.parseInt(st.nextToken());
+		parents = new int[n+1];
+		
+		for (int i = 1; i <= n; i++) {
+			parents[i] = i;
+		}
 		
 		st = new StringTokenizer(br.readLine());
 		int m = Integer.parseInt(st.nextToken());
 		
-		adjList = new ArrayList[n+1]; 
-		for (int i = 1; i <= n; i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		
+		edgeList = new Edge[m];
 		for (int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
 			
-			adjList[u].add(new Node(v, cost));
-			adjList[v].add(new Node(u, cost));
+			Edge edge = new Edge(u, v, w);
+			edgeList[i] = edge;
 		}
 		
-		visited = new boolean[n+1];
-		answer = 0;
-		prim(1);
-		sb.append(answer).append("\n");
-		System.out.println(sb);
-	}
-	
-	static void prim(int start) {
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) ->  {
+		Arrays.sort(edgeList, (o1, o2) -> {
 			return Integer.compare(o1.cost, o2.cost);
 		});
-		pq.add(new Node(start, 0));
 		
-		while(!pq.isEmpty()) {
-			Node now = pq.poll();
+		int answer = 0;
+		int count = 0;
+		for (int i = 0; i < m; i++) {
+			Edge now = edgeList[i];
 			
-			if(visited[now.end]) continue;
-			visited[now.end] = true;
-			
-			answer += now.cost;
-			
-			for (int i = 0; i < adjList[now.end].size(); i++) {
-				Node next = adjList[now.end].get(i);
-				
-				if(!visited[next.end]) {
-					pq.add(next);
-				}
+			if(union(now.start, now.end)) {
+				answer += now.cost;
+				count ++;
 			}
+			
+			if(count == n-1) break;
 		}
 		
+		sb.append(answer).append("\n");
+		System.out.println(sb);
+		
+	}
+	
+	static boolean union(int a, int b) {
+		int x = find(a);
+		int y = find(b);
+		
+		if(x == y) {
+			return false;
+		}else {
+			parents[x] = y;
+			return true;
+		}
+		
+	}
+	
+	static int find(int a) {
+		if(parents[a] == a) return parents[a];
+		return parents[a] = find(parents[a]);
 	}
 }
