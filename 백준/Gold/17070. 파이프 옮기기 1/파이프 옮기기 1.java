941ddef30c9e7a8543067e31e823690974a6612c
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-02-29 00:10:59 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 352 ms, Memory: 164032 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -4,126 +4,65 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
-	static int n;
-	static int[][] board;
-	static int count;
-	static int[] ry = {0, 1, 1};
-	static int[] rx = {1, 0, 1};
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(st.nextToken());
-		
-		board = new int[n][n];
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < n; j++) {
-				
-				int num = Integer.parseInt(st.nextToken());
-				if(num != 0) {
-					board[i][j] = num;
-				}
-			}
-		}
-		// 0 빈칸 // 1 벽
-		
-		count = 0;		
-		go(new int[] {0, 1}, 0);
-		System.out.println(count);
-	}
-	
-	static void go(int[] now, int d) {
-		if(now[0] == n-1 && now[1] == n-1) {
-			count ++;
-			return;
-		}
-		
-		if(d == 0) {
-			// 가로면
-			// 가로 혹은 대각선
-			int flag = 0;
-			for (int i = 0; i < 2; i++) {
-				int r = now[0] + ry[i];
-				int c = now[1] + rx[i];
-				
-				if(r < 0 || c < 0 || r>= n || c>= n ) continue;
-				
-				if(board[r][c] == 1) continue;
-				
-				flag ++;
-				
-				if(i == 1) continue;
-			
-				go(new int[] {r, c}, 0);
-			}
-			
-			if(flag == 2) {
-				int r = now[0] + ry[2];
-				int c = now[1] + rx[2];
-				
-				if(board[r][c] != 1 ) {
-					go(new int[] {r, c}, 2);
-				}
-			}
-			
-		}else if(d == 1) {
-			// 세로면
-			// 세로 혹은 대각선
-			
-			int flag = 0;
-			for (int i = 0; i < 2; i++) {
-				int r = now[0] + ry[i];
-				int c = now[1] + rx[i];
-				
-				if(r < 0 || c < 0 || r>= n || c>= n ) continue;
-				
-				if(board[r][c] == 1) continue;
-				
-				flag ++;
-				
-				if(i == 0) continue;
-			
-				go(new int[] {r, c}, 1);
-			}
-			
-			if(flag == 2) {
-				int r = now[0] + ry[2];
-				int c = now[1] + rx[2];
-				
-				if(board[r][c] != 1 ) {
-					go(new int[] {r, c}, 2);
-				}
-			}
-			
-		}else if(d == 2) {
-			// 대각선이면
-			// 가로 혹은 세로 혹은 대각선
-			int flag = 0;
-			for (int i = 0; i < 2; i++) {
-				int r = now[0] + ry[i];
-				int c = now[1] + rx[i];
-				
-				if(r < 0 || c < 0 || r>= n || c>= n ) continue;
-				
-				if(board[r][c] == 1) continue;
-				
-				flag ++;
-			
-				go(new int[] {r, c}, i);
-			}
-			
-			if(flag == 2) {
-				int r = now[0] + ry[2];
-				int c = now[1] + rx[2];
-				
-				if(board[r][c] != 1 ) {
-					go(new int[] {r, c}, 2);
-				}
-			}
-		}
-	}
+    static int n;
+    static int[][] board;
+    static int count;
+    static boolean[][] visited;
+    static int[] ry = {0, 1, 1};
+    static int[] rx = {1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+
+        board = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+
+                int num = Integer.parseInt(st.nextToken());
+                if(num != 0) {
+                    board[i][j] = num;
+                }
+            }
+        }
+        // 0 빈칸 // 1 벽
+
+        count = 0;
+
+        dfs(new int[] {0, 1}, 0);
+        sb.append(count).append("\n");
+        System.out.println(sb);
+    }
+
+    static void dfs(int[] now, int d){
+        if(now[0] == n-1 && now[1] == n-1){
+            count++;
+            return;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if( (d == 0 && i ==1) || (d == 1 && i == 0) ) continue;
+            // 가로일때 세로
+            // 세로일때 가로
+
+            int r = now[0] + ry[i];
+            int c = now[1] + rx[i];
+
+            if(r < 0 || c < 0 || r>= n || c>= n) continue;
+
+            if(board[r][c] == 1) continue;
+
+            if(i== 2){
+                if(board[r][c-1] == 1 || board[r-1][c] == 1) continue;
+            }
+
+            dfs(new int[]{r, c}, i);
+
+        }
+    }
 }
