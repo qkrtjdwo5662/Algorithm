fbbbefe9d953ff7651cf9daf250a76752cbda6fb
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-03-26 12:20:26 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 320 ms, Memory: 22832 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,40 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static class Node{
-		int y;
-		int x;
-		int cost;
-		
-		public Node(int y, int x, int cost) {
-			this.y = y;
-			this.x = x;
-			this.cost = cost;
-		}
-	}
-	static int[][] board;
 	static int n;
+	static int[][] board;
 	static boolean[][] visited;
-	static final int INF = 987654321;
-	static int[] ry = {1, -1, 0, 0};
-	static int[] rx = {0, 0, 1, -1};
-	
+	static int[][] dist;
+	static int[] ry = {0, 0, 1, -1};
+	static int[] rx = {1, -1, 0, 0};
+	static final int INF= 987654321;
+	static int answer;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		n = 0;
-		int tc = 0;
+		int index = 1;
 		
-		while((n = Integer.parseInt(br.readLine())) != 0) {
-			tc ++;
+		while((n= Integer.parseInt(br.readLine())) != 0) {
 			board = new int[n][n];
+			dist = new int[n][n];
+			visited = new boolean[n][n];
+			answer = 0;
 			
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
@@ -44,48 +33,60 @@ public class Main {
 				}
 			}
 			
-		
-			sb.append("Problem ").append(tc).append(": ").append(dijkstra()).append("\n");
+			answer = dijkstra(new int[] {0, 0});
+			
+			sb.append("Problem ");
+			sb.append(index);
+			sb.append(": ");
+			sb.append(answer).append("\n");
+			
+			index ++;
 		}
+		
 		System.out.println(sb);
+		
 	}
 	
-	static int dijkstra() {
-		visited = new boolean[n][n];
-		int[][] dist = new int[n][n];
-		
-		PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2)-> {
-			return Integer.compare(o1.cost, o2.cost);
-		});
+	static int dijkstra(int[] start) {
 		
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
 				dist[i][j] = INF;
 			}
 		}
-		pq.add(new Node(0, 0, board[0][0]));
-		dist[0][0] = board[0][0];
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+			return Integer.compare(o1[2], o2[2]);
+		});
+		
+		dist[start[0]][start[1]] = board[start[0]][start[1]];
+		pq.add(new int[] {start[0], start[1], dist[start[0]][start[1]]});
 		
 		while(!pq.isEmpty()) {
-			Node now = pq.poll();
+			int[] now = pq.poll();
 			
-			if(visited[now.y][now.x]) continue;
-			visited[now.y][now.x] = true;
+			if(now[0] == n-1 && now[1] == n-1) return dist[n-1][n-1];
+			
+			if(visited[now[0]][now[1]]) continue;
+			visited[now[0]][now[1]] = true;
 			
 			for (int i = 0; i < 4; i++) {
-				int r = now.y + ry[i];
-				int c = now.x + rx[i];
+				int r = now[0] + ry[i];
+				int c = now[1] + rx[i];
 				
-				if(r < 0 || c < 0 || r>= n || c>= n) continue;
+				if(r < 0 || c< 0 || r>= n || c>= n) continue;
 				
-				if(dist[r][c] > dist[now.y][now.x] + board[r][c]) {
-					dist[r][c] = dist[now.y][now.x] + board[r][c];
-					pq.add(new Node(r, c, dist[r][c]));
+				if(dist[r][c] > dist[now[0]][now[1]] + board[r][c]) {
+					dist[r][c] = dist[now[0]][now[1]] + board[r][c];
+					pq.add(new int[] {r, c, dist[r][c]});
 				}
 			}
+			
 		}
 		
-		return dist[n-1][n-1];
+		
+//		return dist[n-1][n-1];
+		return -1;
 		
 	}
 }
