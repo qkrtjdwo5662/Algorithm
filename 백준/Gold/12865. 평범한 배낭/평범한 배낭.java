384a0119b2b331b00698d5587168d8cd15c5d98e
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-03-27 11:56:03 +0900

    [Gold V] Title: 평범한 배낭, Time: 112 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,9 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+	
+	static int n;
+	static int k;
+	static int[] dp;
 	
 	static class Good{
 		int weight;
@@ -13,15 +18,11 @@ public class Main {
 			this.weight = weight;
 			this.value = value;
 		}
-		
 	}
 	
-	static int n;
-	static int k;
-	static int[][] dp;
-	static Good[] goods;
+	static Good goods[];
+	
 	public static void main(String[] args) throws IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
@@ -29,34 +30,31 @@ public class Main {
 		n = Integer.parseInt(st.nextToken());
 		k = Integer.parseInt(st.nextToken());
 		
-		dp = new int[n + 1][k + 1];
-		
 		goods = new Good[n];
+		dp = new int[k + 1];
 		
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
-			
 			int weight = Integer.parseInt(st.nextToken());
 			int value = Integer.parseInt(st.nextToken());
+			
 			
 			Good good = new Good(weight, value);
 			goods[i] = good;
 		}
 		
-		
-		for (int i = 1; i <= n; i++) {
-			Good now = goods[i-1];
+		for (int i = 0; i < n; i++) {
+			Good now = goods[i];
 			
-			for (int j = 0; j <= k; j++) {
-				dp[i][j] = dp[i-1][j];
-				
-				if(j>= now.weight) {
-					dp[i][j] = Math.max(dp[i-1][j-now.weight] + now.value, dp[i][j]);
-				}
+			for (int j = k; j >= now.weight; j--) {
+				dp[j] = Math.max(dp[j], dp[j - now.weight] + now.value);
 			}
+			
+//			System.out.print(Arrays.toString(dp));
 		}
 		
-		sb.append(dp[n][k]).append("\n");
+		sb.append(dp[k]).append("\n");
+//		System.out.print(Arrays.toString(dp));
 		System.out.println(sb);
 		
 	}
