9480b9b2b70c655a1298dc341372a0e5c44c3995
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-03-29 09:38:53 +0900

    [Gold V] Title: A와 B 2, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -3,50 +3,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static String T;
-    static String S;
-    static boolean flag;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        S = br.readLine();
-        T = br.readLine();
-
-        flag = false;
-
-        go(new StringBuilder(T));
-        if(flag) {
-            sb.append(1).append("\n");
-        }else sb.append(0).append("\n");
-
-        System.out.println(sb);
-    }
-
-    static void go(StringBuilder now) {
-        if(flag) return;
-
-
-        if(now.length() == S.length()) {
-            if(now.toString().equals(S)) {
-                flag = true;
-            }
-            return;
-        }
-
-        if(now.charAt(0) == 'B'){
-            StringBuilder next = new StringBuilder(now);
-            next.deleteCharAt(0);
-            next.reverse();
-            go(next);
-        }
-
-        if(now.charAt(now.length() - 1) == 'A'){
-            StringBuilder next = new StringBuilder(now);
-            next.deleteCharAt(next.length() - 1);
-            go(next);
-        }
-
-
-    }
+	static String s;
+	static String t;
+	static boolean flag;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		s = br.readLine();
+		t = br.readLine();
+		flag = false;
+		
+		dfs(new StringBuilder(t));
+			
+		if(flag) {
+			sb.append("1").append("\n");
+		}else sb.append("0").append("\n");
+		
+		System.out.println(sb);
+	}
+	
+	static void dfs(StringBuilder now) {
+		if(flag) return;
+		
+		if(now.length() == s.length()) {
+			if(now.toString().equals(s)) {
+				flag = true;
+			}
+			return;
+		}else if(now.length() < s.length()) return;
+		
+		// 뒤 A
+		if(now.charAt(now.length() - 1) == 'A') {
+			StringBuilder next = new StringBuilder(now);
+			next = next.deleteCharAt(next.length() -1);
+			dfs(next);
+		}
+		
+		StringBuilder next = now.reverse();
+		if(next.charAt(next.length() - 1) == 'B') {
+			next = next.deleteCharAt(next.length() - 1);
+			dfs(next);
+		}
+		
+		
+	}
 }
