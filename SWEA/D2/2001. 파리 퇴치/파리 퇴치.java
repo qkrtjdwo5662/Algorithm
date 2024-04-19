46fd30a426800551c712a2d9312feb5e8c3ec1ce
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-04-19 14:29:06 +0900

    [D2] Title: 파리 퇴치, Time: 99 ms, Memory: 22,392 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,51 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		int tc = Integer.parseInt(st.nextToken());
-		
-		for (int i = 1; i <= tc; i++) {
-			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken());
-			int m = Integer.parseInt(st.nextToken());
-			
-			int[][] board = new int[n+1][n+1];
-			
-			for (int j = 1; j <=  n; j++) {
-				st = new StringTokenizer(br.readLine());
-				for (int k = 1; k <= n; k++) {
-					int num = Integer.parseInt(st.nextToken());
-					board[j][k] = num;
-					board[j][k] += board[j-1][k];
-					board[j][k] += board[j][k-1];
-					board[j][k] -= board[j-1][k-1];
-				}
-			}
-			
-			int max = 0;
-			
-			for (int j = 0; j <= n-m; j++) {
-				for (int k = 0; k <= n-m; k++) {
-					int kill = 0;
-					kill = board[j+m][k+m];
-					kill -= board[j][k+m];
-					kill -= board[j+m][k];
-					kill += board[j][k];
-					
-					max= Math.max(kill, max);
-				}
-			}
-			
-			sb.append("#").append(i).append(" ").append(max).append("\n");
-		}
-		System.out.println(sb);
-		
-	}
+    static int[][] board;
+    static int[][] sum;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int tc = Integer.parseInt(st.nextToken());
+
+        for (int t = 1; t <= tc ; t++) {
+            st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            board = new int[n +1][n + 1];
+            sum = new int[n + 1][n + 1];
+            for (int i = 1; i <= n; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 1; j <= n; j++) {
+                    int num = Integer.parseInt(st.nextToken());
+                    board[i][j] = num;
+                    sum[i][j] = num;
+
+                    sum[i][j] += sum[i-1][j];
+                    sum[i][j] += sum[i][j-1];
+                    sum[i][j] -= sum[i-1][j-1];
+                }
+            }
+
+            int max = 0;
+            for (int i = 0; i <= n-m; i++) {
+                for (int j = 0; j <= n-m ; j++) {
+                    int kill = sum[i + m][j + m] - sum[i+m][j]- sum[i][j + m] + sum[i][j];
+                    max = Math.max(max, kill);
+                }
+            }
+            sb.append("#").append(t).append(" ").append(max).append("\n");
+        }
+        System.out.println(sb);
+
+
+    }
 }
