1f829129ceb8a9fd7a3ac87d1520467229c2e3c2
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-04-22 21:03:32 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,25 +1,34 @@
 -- 코드를 작성해주세요
+# select a.ID, b.FISH_NAME, a.LENGTH
+# from 
+# (
+#     select b.ID, a.FISH_TYPE, a.LENGTH
+#     from(
+#         select FISH_TYPE, MAX(LENGTH) LENGTH
+#         from FISH_INFO 
+#         group by FISH_TYPE
+#     )  a
+#     left join 
+#     (
+#         select ID, FISH_TYPE, LENGTH
+#         from FISH_INFO
+#     ) as b
+#     on a.FISH_TYPE = b.FISH_TYPE and a.LENGTH = b.LENGTH
+# ) as a
+# left join FISH_NAME_INFO b
+# on a.FISH_TYPE = b.FISH_TYPE
+# order by a.ID;
+
+
 select a.ID, b.FISH_NAME, a.LENGTH
-from 
-(
-    select b.ID, a.FISH_TYPE, a.LENGTH
-    from(
-        select FISH_TYPE, MAX(LENGTH) LENGTH
-        from FISH_INFO 
-        group by FISH_TYPE
-    )  a
-    left join 
-    (
-        select ID, FISH_TYPE, LENGTH
-        from FISH_INFO
-    ) as b
-    on a.FISH_TYPE = b.FISH_TYPE and a.LENGTH = b.LENGTH
-) as a
-left join FISH_NAME_INFO b
+from FISH_INFO a
+join FISH_NAME_INFO b
 on a.FISH_TYPE = b.FISH_TYPE
-order by a.ID;
+where a.FISH_TYPE in 
+(
+    select FISH_TYPE
+    from FISH_INFO
+    group by FISH_TYPE
+    having LENGTH = max(LENGTH)
+)
 
-
-
-# left join FISH_NAME_INFO n
-# on f.FISH_TYPE = n.FISH_TYPE
