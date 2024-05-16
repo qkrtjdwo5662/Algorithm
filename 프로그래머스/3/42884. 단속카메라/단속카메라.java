f04915595d6195dc34732af989e81511522de207
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-05-16 17:10:29 +0900

    [level 3] Title: 단속카메라, Time: 7.55 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,24 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
+        Arrays.sort(routes, (o1, o2) -> {
+           return Integer.compare(o1[0], o2[0]); 
+        }); // 시작점 기준 정렬
+        
         int answer = 1;
         
-        Arrays.sort(routes, (o1, o2) -> {
-            return Integer.compare(o1[0], o2[0]);
-        });
-        
-        // System.out.println(Arrays.deepToString(routes));
         int start = routes[0][0];
         int end = routes[0][1];
         
         for(int i=1; i<routes.length; i++){
             if(start <= routes[i][0] && routes[i][0] <= end){
-                start = Math.max(start, routes[i][0]);
-                end = Math.min(end, routes[i][1]);
+                start = routes[i][0];
+                end = Math.min(routes[i][1], end);
             }else{
+                answer++;
                 start = routes[i][0];
                 end = routes[i][1];
-                answer ++;
             }
         }
         
