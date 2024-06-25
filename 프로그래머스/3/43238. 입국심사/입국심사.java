2afbfee898671f09c6152f524142af547a274a60
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-06-26 02:00:37 +0900

    [level 3] Title: 입국심사, Time: 0.38 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,27 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
     public long solution(int n, int[] times) {
         long answer = 0;
-        long max = 1_000_000_000;
-        
-        // 7 10 14 20 21 28  
-        long left = 0;
-        long right = max * n;
-        
-        while(left + 1 < right){
-            long center = (left + right) / 2;
-            long check = 0;
-            
-            for(int i=0; i < times.length; i++){
-                check += center / times[i];
+
+        long left = 1;
+         Arrays.sort(times);
+        long right = (long) n *times[0];
+
+        while(left <= right){
+            long mid = (left + right) / 2;
+
+            long count = 0;
+            for (int i = 0; i < times.length; i++) {
+                count += mid / times[i];
             }
-            
-            if(check >= n){
-                right = center;
-            }else{
-                left = center;
+
+            if(count < n){
+                left = mid + 1;
+            }else {
+                answer = mid;
+                right = mid - 1;
             }
         }
-        answer = right;
+
         return answer;
     }
 }
