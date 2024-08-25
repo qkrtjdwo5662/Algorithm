0a536a3ed141c7521e1a2a86c1e2a6147a9d615c
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-08-25 16:35:10 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 12412 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,111 +5,85 @@ import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static char[][] board;
-	static boolean[][] visited;
-	
-	static int[] ry = {0, 0, 1, -1};
-	static int[] rx = {1, -1, 0, 0};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(st.nextToken());
-		board = new char[n][n];
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			for (int j = 0; j < n; j++) {
-				board[i][j] = s.charAt(j);
-			}
-		}
-		
-		visited = new boolean[n][n];
-		int count = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(!visited[i][j]) {
-					normal(new int[] {i, j}, board[i][j]);
-					count++;
-				}
-			}
-		}
-		sb.append(count).append(" ");
-		
-		visited = new boolean[n][n];
-		count = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if(!visited[i][j]) {
-					blindColor(new int[] {i, j}, board[i][j]);
-					count++;
-				}
-			}
-		}
-		sb.append(count).append("\n");
-//		
-		System.out.println(sb);
-	}
-	static void normal(int[] start, char color) {
-		visited[start[0]][start[1]] = true;
-		ArrayDeque<int[]> deque = new ArrayDeque<>();
-		
-		deque.addLast(start);
-		
-		while(!deque.isEmpty()) {
-			int[] now =deque.pollFirst();
-			
-			for (int i = 0; i < 4; i++) {
-				int r = now[0] + ry[i];
-				int c = now[1] + rx[i];
-				
-				if(r < 0 || c<0 || r>= n || c>= n) continue;
-				
-				if(board[r][c] != color) continue;
-				
-				if(!visited[r][c]) {
-					visited[r][c] = true;
-					deque.addLast(new int[] {r, c});
-				}
-			}
-			
-		}
-		
-	}
-	
-	static void blindColor(int[] start, char color) {
-		visited[start[0]][start[1]] = true;
-		ArrayDeque<int[]> deque = new ArrayDeque<>();
-		
-		deque.addLast(start);
-		
-		while(!deque.isEmpty()) {
-			int[] now =deque.pollFirst();
-			
-			for (int i = 0; i < 4; i++) {
-				int r = now[0] + ry[i];
-				int c = now[1] + rx[i];
-				
-				if(r < 0 || c<0 || r>= n || c>= n) continue;
-				
-				if(color == 'R') {
-					if(board[r][c] == 'B' ) continue;
-				}else if(color == 'G') {
-					if(board[r][c] == 'B' ) continue;
-				}else if(color == 'B') {
-					if(board[r][c] != 'B' ) continue;
-				}
-				
-				
-				if(!visited[r][c]) {
-					visited[r][c] = true;
-					deque.addLast(new int[] {r, c});
-				}
-			}
-			
-		}
-	}
+    static int n;
+    static int[][] map;
+    static int[][] handicapMap;
+    static boolean[][] visited;
+    static ArrayDeque<int[]> deque;
+    static int[] ry = {0, 0, 1, -1};
+    static int[] rx = {1, -1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][n];
+        handicapMap = new int[n][n];
+        deque = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String s = st.nextToken();
+            for (int j = 0; j < n; j++) {
+                if(s.charAt(j) == 'G'){
+                    map[i][j] = 1;
+                }else if(s.charAt(j) == 'B'){
+                    map[i][j] = 2;
+                    handicapMap[i][j] = 2;
+                }
+            }
+        }
+
+        int answer = 0;
+        visited = new boolean[n][n];
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < n; j++) {
+                if(!visited[i][j]){
+                    bfs(i, j, map);
+                    answer ++;
+                }
+            }
+        }
+        sb.append(answer).append(" ");
+
+        answer = 0;
+        visited = new boolean[n][n];
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < n; j++) {
+                if(!visited[i][j]){
+                    bfs(i, j, handicapMap);
+                    answer ++;
+                }
+            }
+        }
+
+        sb.append(answer).append("\n");
+        System.out.println(sb);
+    }
+
+    static void bfs(int startY, int startX, int[][] map){
+        visited[startY][startX] = true;
+        deque.addLast(new int[]{startY, startX});
+
+        int num = map[startY][startX];
+        while(!deque.isEmpty()){
+            int[] now = deque.pollFirst();
+
+            for (int i = 0; i < 4; i++) {
+                int r = now[0] + ry[i];
+                int c = now[1] + rx[i];
+
+                if(r < 0  || c < 0 || r>=n || c>=n) continue;
+
+                if(map[r][c] != num) continue;
+
+                if(visited[r][c]) continue;
+
+                visited[r][c] = true;
+                deque.addLast(new int[]{r, c});
+            }
+        }
+    }
 }
