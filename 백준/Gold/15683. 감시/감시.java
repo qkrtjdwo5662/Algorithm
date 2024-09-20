08da2202bb3681ab2f80210de2ad681301404b78
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-09-20 21:45:36 +0900

    [Gold III] Title: 감시, Time: 172 ms, Memory: 27884 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,322 +1,259 @@
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int m;
-	static int[][] board;
-	static ArrayList<int[]> cctvList;
-	
-	static int[] ry = {0, -1, 0, 1};
-	static int[] rx = {1, 0, -1, 0};
-	
-	static int answer;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		board = new int[n][m];
-		cctvList = new ArrayList<>();
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				board[i][j] = num;
-				
-				if(num >= 1 && num <= 5) cctvList.add(new int[] {i, j}); 
-			}
-		}
-		answer = 987654321;
-		run(0);
-		System.out.println(answer);
-	}
-	
-	static int countSafeArea() {
-		int count = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if(board[i][j] == 0) count++;
-			}
-		}
-		
-		return count;
-	}
-	static void printBoard() {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				System.out.print(board[i][j] + " ");
-			}
-			System.out.println();
-		}
-		System.out.println();
-	}
-	
-	static void run(int index) {
-		if(index == cctvList.size()) {
-			answer= Math.min(answer, countSafeArea());
-			return;
-		}
-		
-		int[] now = cctvList.get(index);
-		int num = board[now[0]][now[1]];
-		
-		if(num == 1) { // 우
-			for (int i = 0; i < 4; i++) {
-				int r = now[0];
-				int c = now[1];
-				
-				ArrayList<int[]> checkList = new ArrayList<>();
-				while(true) {
-					r += ry[i];
-					c += rx[i];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				run(index+1);
-				back(checkList);
-			}
-		}else if(num == 2) { // 좌우
-			for (int i = 0; i < 2; i++) {
-				int r = now[0];
-				int c = now[1];
-				
-				ArrayList<int[]> checkList = new ArrayList<>();
-				while(true) {
-					r += ry[i];
-					c += rx[i];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				r = now[0];
-				c = now[1];
-				while(true) {
-					r += ry[i + 2];
-					c += rx[i + 2];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				run(index+1);
-				back(checkList);
-			}
-		}else if(num == 3) { // 상우
-			for (int i = 0; i < 4; i++) {
-				int r = now[0];
-				int c = now[1];
-				
-				ArrayList<int[]> checkList = new ArrayList<>();
-				while(true) {
-					r += ry[i];
-					c += rx[i];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				r = now[0];
-				c = now[1];
-				while(true) {
-					r += ry[(i+1) % 4];
-					c += rx[(i+1) % 4];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				run(index+1);
-				back(checkList);
-			}
-		}else if(num == 4) { // 좌상우
-			for (int i = 0; i < 4; i++) {
-				int r = now[0];
-				int c = now[1];
-				
-				ArrayList<int[]> checkList = new ArrayList<>();
-				while(true) {
-					r += ry[i];
-					c += rx[i];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				r = now[0];
-				c = now[1];
-				while(true) {
-					r += ry[(i+1) % 4];
-					c += rx[(i+1) % 4];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				r = now[0];
-				c = now[1];
-				while(true) {
-					r += ry[(i+2) % 4];
-					c += rx[(i+2) % 4];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-				
-				run(index+1);
-				back(checkList);
-			}
-		}else if(num == 5) { // 좌상우하
-			ArrayList<int[]> checkList = new ArrayList<>();
-			for (int i = 0; i < 4; i++) {
-				int r = now[0];
-				int c = now[1];
-				
-				while(true) {
-					r += ry[i];
-					c += rx[i];
-					
-					if(r < 0 || c< 0 || r>=n || c>= m ) break;
-					// 맵 밖 벗어나면 break;
-					
-					if(board[r][c] == 6) break;
-					// 벽이면 멈춤
-					
-					if(board[r][c] == -1 || (board[r][c] >= 1 && board[r][c] <=6 )) continue;
-					// 이미 감시영역이거나 cctc면 넘어감
-					
-					if(board[r][c] == 0) {
-						board[r][c] = -1;
-						// 빈칸이면 영역표시
-						
-						checkList.add(new int[] {r, c});
-					}
-				}
-			}
-			
-			run(index+1);
-			back(checkList);
-		}
-	}
-	
-	static void back(ArrayList<int[]> checkList) {
-		for (int i = 0; i < checkList.size(); i++) {
-			int[] now = checkList.get(i);
-			
-			board[now[0]][now[1]] = 0;
-		}
-	}
+    static int n;
+    static int m;
+    static int[][] map;
+    static List<int[]> cctvList;
+    static int[] ry = {0, 1, 0 ,-1};
+    static int[] rx = {1, 0, -1, 0};
+    static int answer = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+
+        cctvList = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                map[i][j] = num;
+
+                if(num >= 1 && num <= 5) cctvList.add(new int[]{i, j});
+            }
+        }
+
+        backtrack(0);
+        sb.append(answer).append("\n");
+        System.out.println(sb);
+    }
+
+    static void backtrack(int depth){
+        if(depth == cctvList.size()){
+            answer = Math.min(answer, count());
+            return;
+        }
+
+        int[] now = cctvList.get(depth);
+        int cctvNum = map[now[0]][now[1]];
+        List<int[]> backupList = new ArrayList<>();
+
+        cctvRun(cctvNum, backupList, now, depth);
+
+    }
+
+    static void cctvRun(int num, List<int[]> list, int[] now, int depth){
+        int nowY = now[0];
+        int nowX = now[1];
+
+        if(num == 1){
+            for (int i = 0; i < 4; i++) {
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[i];
+                    int c = nowX + rx[i];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+
+                }
+                backtrack(depth + 1);
+                backup(list);
+            }
+        }else if(num == 2){
+            for (int i = 0; i < 2; i++) {
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[i];
+                    int c = nowX + rx[i];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[i + 2];
+                    int c = nowX + rx[i + 2];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+                backtrack(depth + 1);
+                backup(list);
+            }
+        }else if(num == 3){
+            for (int i = 0; i < 4; i++) {
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[i];
+                    int c = nowX + rx[i];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[(i + 1) % 4];
+                    int c = nowX + rx[(i + 1) % 4];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+                backtrack(depth + 1);
+                backup(list);
+            }
+        }else if(num == 4){
+            for (int i = 0; i < 4; i++) {
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[i];
+                    int c = nowX + rx[i];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[(i + 1) % 4];
+                    int c = nowX + rx[(i + 1) % 4];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+
+                nowY = now[0];
+                nowX = now[1];
+                while(true){
+                    int r = nowY + ry[(i + 2) % 4];
+                    int c = nowX + rx[(i + 2) % 4];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+                backtrack(depth + 1);
+                backup(list);
+            }
+        }else if(num == 5){
+            for (int i = 0; i < 4; i++) {
+                nowY = now[0];
+                nowX = now[1];
+
+                while(true){
+                    int r = nowY + ry[i];
+                    int c = nowX + rx[i];
+
+                    if(r < 0 || c < 0 || r>= n || c>= m) break;
+                    if(map[r][c] == 6) break;
+                    if(map[r][c] == 0){
+                        map[r][c] = -1;
+                        list.add(new int[]{r, c});
+                    }
+
+                    nowY = r;
+                    nowX = c;
+                }
+            }
+
+            backtrack(depth + 1);
+            backup(list);
+        }
+    }
+
+    static void backup(List<int[]> list){
+        for (int i = 0; i < list.size(); i++) {
+            int[] now = list.get(i);
+
+            map[now[0]][now[1]] = 0;
+        }
+    }
+
+    static int count(){
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(map[i][j] == 0) answer ++;
+            }
+        }
+
+        return answer;
+    }
 }
