a1020f2fd43c8a34a4b45323b2cd1665544f45a4
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-09-21 00:58:20 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,13 +1,13 @@
-# 테이블에서 조회수가 가장 높은 중고거래 게시물
-select concat('/home/grep/src/',b.BOARD_ID, '/', f.FILE_ID, f.FILE_NAME, f.FILE_EXT) as FILE_PATH
-from USED_GOODS_BOARD b
-join USED_GOODS_FILE f
-on b.BOARD_ID = f.BOARD_ID
-WHERE 
-    b.BOARD_ID = (
-        SELECT b2.BOARD_ID
-        FROM USED_GOODS_BOARD b2
-        ORDER BY b2.VIEWS DESC
+select concat('/home/grep/src/', a.BOARD_ID, '/', b.FILE_ID, b.FILE_NAME, b.FILE_EXT) as FILE_PATH
+from USED_GOODS_BOARD a
+join USED_GOODS_FILE b
+on a.BOARD_ID = b.BOARD_ID
+where 
+    a.BOARD_ID = (
+        select BOARD_ID 
+        from USED_GOODS_BOARD 
+        ORDER BY VIEWS DESC 
         LIMIT 1
     )
-order by f.FILE_ID desc
+    
+order by b.FILE_ID desc;
