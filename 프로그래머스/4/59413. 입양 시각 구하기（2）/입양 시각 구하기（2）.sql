1c91581d59ebc1baa6a37c2752769a439877d7dd
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-16 00:58:44 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+# SELECT HOUR(DATETIME) as HOUR, COUNT(HOUR(DATETIME)) as COUNT
+# from ANIMAL_OUTS
+# group by HOUR
+# order by HOUR
+
+set @hour := -1;
+
+select (@hour := @hour + 1) as HOUR, (
+    select count(*)
+    from ANIMAL_OUTS
+    where HOUR(DATETIME) = @hour) as COUNT
+
+from ANIMAL_OUTS
+WHERE @hour < 23;
