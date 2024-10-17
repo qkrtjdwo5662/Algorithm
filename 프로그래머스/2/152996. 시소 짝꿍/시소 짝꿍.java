fbde98145cfb302a0ad86519f1ea847346ce8092
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-17 18:18:35 +0900

    [level 2] Title: 시소 짝꿍, Time: 43.29 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -3,28 +3,37 @@ import java.util.*;
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-        int n = weights.length;
+        Map<Integer, Integer> map = new HashMap<>();
         
-        Arrays.sort(weights);
+        for(int num : weights){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
         
-        Map<Double, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<weights.length; i++){
-            double num = weights[i];
+        for(int num : map.keySet()){
+            int count = map.get(num);
             
-            double a = num;
-            
-            double b = num / 2;
-            
-            double c = num * 3/4;
-            
-            double d = num * 2/3;
-            if(map.containsKey(a)) answer += map.get(a);
-            if(map.containsKey(b)) answer += map.get(b);
-            if(map.containsKey(c)) answer += map.get(c);
-            if(map.containsKey(d)) answer += map.get(d);
-            
-            map.put(a, map.getOrDefault(a, 0) + 1);
+            // 같은 무게를 가진 경우
+            if(count > 1) answer += (long)count * (count - 1) / 2;
+
+            // 비율 계산
+            int b = num * 2;
+            if(b <= 1000 && map.containsKey(b)){
+                answer += (long)count * map.get(b);
+            }
+
+            if(num % 3 == 0) {
+                int c = num * 4 / 3;
+                if(c <= 1000 && map.containsKey(c)){
+                    answer += (long)count * map.get(c);
+                }
+            }
+
+            if(num % 2 == 0) {
+                int d = num * 3 / 2;
+                if(d <= 1000 && map.containsKey(d)){
+                    answer += (long)count * map.get(d);
+                }
+            }
         }
         
         return answer;
