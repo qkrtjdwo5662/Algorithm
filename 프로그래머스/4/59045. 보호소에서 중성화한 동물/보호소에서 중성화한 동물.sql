bf924e2b8d663bd9c83015e39effc243744361b4
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-17 23:43:09 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT a.ANIMAL_ID, a.ANIMAL_TYPE, a.NAME
+from ANIMAL_INS a
+left join ANIMAL_OUTS b
+on a.ANIMAL_ID = b.ANIMAL_ID
+where a.SEX_UPON_INTAKE != b.SEX_UPON_OUTCOME
+order by a.ANIMAL_ID
