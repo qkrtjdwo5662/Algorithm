bd54adcc3c216d2166129f37452ccf5a3ab36124
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-17 11:28:43 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,13 +1,12 @@
 select ID
 from ECOLI_DATA
-where PARENT_ID IN
-(
+where PARENT_ID in(
     select ID
     from ECOLI_DATA
-    where PARENT_ID IN
-    (
+        where PARENT_ID in (
         select ID
         from ECOLI_DATA
         where PARENT_ID is null
-    )
+    )  
 )
+order by ID;
