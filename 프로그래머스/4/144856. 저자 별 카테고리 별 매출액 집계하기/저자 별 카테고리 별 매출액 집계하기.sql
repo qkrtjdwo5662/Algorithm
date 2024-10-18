31211f064f8a52b94ea222cf6ca64b9aeedcebcb
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-18 19:52:36 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,16 +1,10 @@
 -- 코드를 입력하세요
-SELECT b.AUTHOR_ID, b.AUTHOR_NAME, a.CATEGORY, sum(c.SALES * a.PRICE)  as TOTAL_SALES
-from BOOK a
-left join AUTHOR b
+SELECT a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, SUM(c.SALES * b.PRICE) as TOTAL_SAELS
+from AUTHOR as a
+left join BOOK b
 on a.AUTHOR_ID = b.AUTHOR_ID
 left join BOOK_SALES c
-on c.BOOK_ID = a.BOOK_ID
-where DATE_FORMAT(c.SALES_DATE, '%Y-%m') = '2022-01'
-group by b.AUTHOR_ID, a.CATEGORY
-order by b.AUTHOR_ID, a.CATEGORY desc;
-
-# select *
-# from BOOK a
-# left join BOOK_SALES c
-# on c.BOOK_ID = a.BOOK_ID
-# where DATE_FORMAT(c.SALES_DATE, '%Y-%m') = '2022-01'
+on b.BOOK_ID = c.BOOK_ID
+where YEAR(c.SALES_DATE) = 2022 and MONTH(c.SALES_DATE) = 1
+group by a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY
+order by a.AUTHOR_ID , b.CATEGORY desc;
