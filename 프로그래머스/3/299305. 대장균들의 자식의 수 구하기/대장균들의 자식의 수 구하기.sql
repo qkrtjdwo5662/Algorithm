1e826de068f14e8ca4a608795013b22747060408
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-10-19 01:51:50 +0900

    [level 3] Title: 대장균들의 자식의 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
+++ 프로그래머스/3/299305. 대장균들의 자식의 수 구하기/대장균들의 자식의 수 구하기.sql
@@ -1,14 +1,15 @@
 -- 코드를 작성해주세요
 select a.ID, 
-case 
-    when b.count is not null then b.count
-    else 0
+case
+when b.count is null then 0
+else b.count
 end as CHILD_COUNT
-from ECOLI_DATA a
-left join (
-select PARENT_ID, count(PARENT_ID) as count
-from ECOLI_DATA
-where PARENT_ID is not null
-group by PARENT_ID) as b
+from ECOLI_DATA as a
+left join 
+(
+    select PARENT_ID, count(PARENT_ID) as count
+    from ECOLI_DATA 
+    where PARENT_ID is not null
+    group by PARENT_ID
+) as b
 on a.ID = b.PARENT_ID
-order by a.ID
