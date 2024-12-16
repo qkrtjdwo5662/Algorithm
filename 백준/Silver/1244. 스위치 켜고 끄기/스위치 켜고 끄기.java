fd8e50dc0d237fb1b8f99a1f0278c4c989c6e55f
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-12-16 11:53:04 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 60 ms, Memory: 11612 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,106 +1,74 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+    static int n;
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		int n = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
 
-		int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
-		st = new StringTokenizer(br.readLine());
-		for (int i = 1; i <= n; i++) {
-			int el = Integer.parseInt(st.nextToken());
-			arr[i] = el;
-		}
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int status = Integer.parseInt(st.nextToken()); // m or wm
+            int num = Integer.parseInt(st.nextToken()); // num
+            go(status, num);
+        }
 
-		st = new StringTokenizer(br.readLine());
+        int index = 20;
+        for (int i = 1; i <= n ; i++) {
+            sb.append(arr[i]).append(" ");
+            if(i % index == 0) sb.append("\n");
+        }
+        sb.append("\n");
+        System.out.println(sb);
 
-		int m = Integer.parseInt(st.nextToken());
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int gender = Integer.parseInt(st.nextToken());
-			// 1:남 2:여
+    }
 
-			int num = Integer.parseInt(st.nextToken());
-			// 받는 수
+    static void go(int status, int num){
+        if(status == 1){ // m
+            int index = num;
+            while(index <= n){
+                if(arr[index] == 0){
+                    arr[index] = 1;
+                }else arr[index] = 0;
 
-			// 남자는 받는 수의 배수의 상태 바꿈
-			// 여자는 받는 수를 위치로
+                index += num;
+            }
+        }else if(status == 2){ // wm
+            int index = num;
+            int a = 1;
+            int s = 0;
+            int e = 0;
 
-			run(gender, num, arr, n);
-		}
+            while(index + a <=n && index - a > 0){
+                if(arr[index + a] != arr[index - a]) break;
 
-		if (n > 20) {
-			int index = 1;
+                a++;
+            }
 
-			while (index < n) {
-				if (index % 20 == 0) {
-					sb.append(arr[index]);
-					sb.append("\n");
-				} else {
-					sb.append(arr[index]).append(" ");
-				}
-				index++;
-			}
-			sb.append(arr[n]).append("\n");
-			
-		} else {
-			for (int i = 1; i < n; i++) {
-				sb.append(arr[i]).append(" ");
-			}
-			sb.append(arr[n]).append("\n");
-		}
-
-		System.out.println(sb);
-
-	}
-
-	public static void run(int gender, int num, int[] arr, int n) {
-		if (gender == 1) {
-			// 남자
-			int index = 1;
-
-			while (true) {
-				if (num * index > n)
-					break;
-
-				if (arr[num * index] == 1) {
-					arr[num * index] = 0;
-				} else {
-					arr[num * index] = 1;
-				}
-				index++;
-			}
-		} else if (gender == 2) {
-			// 여자
-			int index = 0;
-
-			while (true) {
-				if (num - (index + 1) < 1 || num + (index + 1) > n) {
-					break;
-				}
-
-				if (arr[num - (index + 1)] != arr[num + (index + 1)]) {
-					break;
-				}
-
-				index++;
-			}
-
-			for (int i = num - index; i <= num + index; i++) {
-				if (arr[i] == 1) {
-					arr[i] = 0;
-				} else
-					arr[i] = 1;
-			}
-
-		}
-
-	}
+            a--;
+            s = index - a;
+            e = index + a;
+            for (int i = s; i <= e ; i++) {
+                if(arr[i] == 0){
+                    arr[i] = 1;
+                }else arr[i] = 0;
+            }
+        }
+    }
 }
