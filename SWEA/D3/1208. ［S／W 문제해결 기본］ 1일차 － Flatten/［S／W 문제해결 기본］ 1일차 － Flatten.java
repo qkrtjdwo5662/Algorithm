98cba2c718df359a8e05ee23894dae9e97f13ec8
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2024-12-23 13:00:23 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 124 ms, Memory: 21,928 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,37 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 1; i <= 10; i++) {
-			st = new StringTokenizer(br.readLine());
-			int count = Integer.parseInt(st.nextToken());
-			
-			int[] arr = new int[100];
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 100; j++) {
-				int num = Integer.parseInt(st.nextToken());
-				arr[j] = num;
-			}
-			
-			for (int j = 0; j < count; j++) {
-				Arrays.sort(arr);
-				arr[99] --;
-				arr[0] ++;
-			}
-			Arrays.sort(arr);
-			int answer = arr[99] - arr[0];
-			sb.append("#").append(i).append(" ").append(answer).append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+    static final int WIDTH = 100;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int tc = 10;
+        PriorityQueue<Integer> maxHeap;
+        PriorityQueue<Integer> minHeap;
+        for (int t = 1; t <= tc; t++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+
+            maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+            minHeap = new PriorityQueue<>();
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < WIDTH; i++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                maxHeap.add(num);
+                minHeap.add(num);
+            }
+
+            for (int i = 0; i < n; i++) {
+                int max = maxHeap.poll();
+                int min = minHeap.poll();
+
+                max -= 1;
+                min += 1;
+
+                maxHeap.add(max);
+                minHeap.add(min);
+            }
+
+            int answer = Math.abs(maxHeap.peek() - minHeap.peek());
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
 }
