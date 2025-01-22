a7b5cea29271be253614a475a7482718c0a0e0a9
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-01-22 18:06:39 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 158 ms, Memory: 44,428 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,76 +1,90 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int n;
-    static int[][] board;
+    static int[][] map;
     static boolean[] visited;
     static int answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         int tc = Integer.parseInt(st.nextToken());
-
-        for (int i = 1; i <= tc ; i++) {
+        for (int t = 1; t <= tc ; t++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
-            board = new int[n][n];
-            visited = new boolean[n];
+            map = new int[n][n];
             answer = Integer.MAX_VALUE;
-            for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int k = 0; k < n; k++) {
+                for (int j = 0; j < n; j++) {
                     int num = Integer.parseInt(st.nextToken());
-                    board[j][k] = num;
+                    map[i][j] = num;
                 }
             }
+            visited = new boolean[n];
 
-            divide(0, 0);
-            sb.append("#").append(i).append(" ").append(answer).append("\n");
+            dfs(0, 0);
+
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
         }
         System.out.println(sb);
 
-
     }
-    static void sum(){
-        int totalA = 0;
-        int totalB = 0;
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i; j < n; j++) {
-                if(visited[i] && visited[j]){
-                    totalA += board[i][j];
-                    totalA += board[j][i];
-                }
-
-                if(!visited[i] && !visited[j]){
-                    totalB += board[i][j];
-                    totalB += board[j][i];
-                }
-            }
-        }
-        int abs = Math.abs(totalA - totalB);
-        answer = Math.min(answer, abs);
-
-    }
-    static void divide(int depth, int index){
-        if(depth == n/2){
-            sum();
+    static void dfs(int depth, int count){
+        if(count == n/2){
+            answer = Math.min(cal(), answer);
             return;
         }
 
-        for (int i = index; i < n; i++) {
-            if(!visited[i]){
-                visited[i] = true;
-                divide(depth + 1, i);
-                visited[i] = false;
+        if(depth == n){
+            return;
+        }
+
+        visited[depth] = true;
+        dfs(depth + 1, count + 1);
+        visited[depth] = false;
+        dfs(depth + 1, count);
+    }
+
+    static int cal(){
+        int[] A = new int[n/2];
+        int[] B = new int[n/2];
+
+        int indexA = 0;
+        int indexB = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(visited[i]){
+                A[indexA] = i;
+                indexA ++;
+            }else{
+                B[indexB] = i;
+                indexB ++;
             }
         }
+
+        int totalA = 0;
+        int totalB = 0;
+        for (int i = 0; i < n/2 - 1; i++) {
+            int a1 = A[i];
+            int b1 = B[i];
+            for (int j = i + 1; j < n/2 ; j++) {
+                int a2 = A[j];
+                int b2 = B[j];
+
+                totalA += map[a1][a2] + map[a2][a1];
+                totalB += map[b1][b2] + map[b2][b1];
+            }
+        }
+
+        return Math.abs(totalA - totalB);
     }
 }
