f6c32a728adb2b0905d813dfa64e11b01136996b
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-02-05 21:18:13 +0900

    [level 2] Title: 양궁대회, Time: 7662.67 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -3,37 +3,46 @@ import java.util.*;
 class Solution {
     static int[] info;
     static int[] answer;
-    static int max = -1;
+    static int max = 0;
+    static int minScore = 11;
     
     public int[] solution(int n, int[] info) {
         answer = new int[11];
         this.info = info; // 어피치
         
-        backtrack(0, n, new int[11]);
-        
-        if(max == -1){
+        backtrack(0, n, new int[11], 10);
+        if(minScore == 11){
            return new int[]{-1}; 
         }
-        
         return answer;
     }
     
-    static void backtrack(int depth, int n, int[] lion){
+    static void backtrack(int depth, int n, int[] lion, int min){
         if(depth == n){
             int diff = calScore(lion);
             
-            if(diff > 0 && diff >= max){ // 갱신되고
-                max = diff;
-                answer = lion.clone();
+            if(diff >= max ){ // 갱신되고
+                if(minScore > min){ // 갱신되면
+                    max = diff;
+                    minScore = min;
+                    
+                    for(int i=0; i<11; i++){
+                        answer[i] = lion[i];
+                    }
+                    
+                }
             }
         
+            
             return;
         }
         
-        for(int i=0; i < 11 && lion[i] <= info[i]; i++){
-            lion[i] ++;
-            backtrack(depth + 1, n, lion);
-            lion[i] --;
+        for(int i=0; i < 11; i++){
+            if(lion[i] <= info[i]){ // 작거나 같은 곳만 ++
+                lion[i] ++;
+                backtrack(depth + 1, n, lion, Math.min(min, 10 - i));
+                lion[i] --;
+            }
         }
     }
     
