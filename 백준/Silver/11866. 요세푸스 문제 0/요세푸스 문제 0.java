ada986c232db2d482fb6403e594ca8d5c896bfa7
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-04-04 00:37:25 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 108 ms, Memory: 12060 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,34 +8,48 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
-        // queue addLast - pollFirst
+        int[] arr = new int[n];
 
-        for (int i = 1; i <= n ; i++) {
-            arrayDeque.addLast(i);
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
         }
+        boolean[] exact = new boolean[n];
 
+        int exactCount = 0;
+        int index = 0; // 배열 인덱스
+        int count = 1; // 몇 번째?
         sb.append("<");
-
-        int count = 0;
-        while(arrayDeque.size() > 1){
-            int now = arrayDeque.pollFirst();
-            count++;
+        while(exactCount < n){
 
             if(count == k){
-                sb.append(now).append(", ");
-                count = 0;
-            }else{
-                arrayDeque.addLast(now);
-            }
-        }
-        sb.append(arrayDeque.pollFirst());
-        sb.append(">");
-        System.out.print(sb);
+                if(!exact[index]){
+                    exact[index] = true;
+                    count = 1;
+                    exactCount ++;
+                    if(exactCount <= n - 1) sb.append(arr[index]).append(", ");
+                    else sb.append(arr[index]).append(">");
+                    index = (index + 1)%n;
 
+
+                    continue;
+                }
+            }
+
+            if(!exact[index]){
+                count ++;
+            }
+
+            index = (index + 1)%n;
+        }
+        // 1 2 3 4 5 6 7
+
+        // 1.. 2.. 3
+        // 4.. 5.. 6
+        // 7.. 1.. 2
+        // 4.. 5 .. 7
+        System.out.println(sb);
     }
 }
