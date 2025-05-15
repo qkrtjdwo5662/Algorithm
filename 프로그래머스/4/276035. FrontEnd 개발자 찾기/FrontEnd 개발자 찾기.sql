10893eec204cfe699faad69ed939aa0062a8c693
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-05-15 17:35:35 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,9 @@
+-- 코드를 작성해주세요
+select ID, EMAIL, FIRST_NAME, LAST_NAME
+from DEVELOPERS
+where SKILL_CODE & (
+    select sum(CODE)
+    from SKILLCODES
+    where CATEGORY = 'Front End')
+order by ID;
+    
