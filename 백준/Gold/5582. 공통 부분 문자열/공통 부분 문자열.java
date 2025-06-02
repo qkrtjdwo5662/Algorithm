95eaa6e1a13b0b2fdf77532a32868cf3d3fe59ce
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-06-02 10:35:00 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 200 ms, Memory: 74748 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,19 +11,23 @@ public class Main {
         String s1 = br.readLine();
         String s2 = br.readLine();
 
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+
+        // lcs
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+
         int answer = 0;
-        for (int i = 1; i <= s1.length() ; i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                char c1 = s1.charAt(i - 1);
-                char c2 = s2.charAt(j - 1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
 
-                if(c1 == c2) dp[i][j] = dp[i - 1][j - 1] + 1;
-
-                answer = Math.max(answer, dp[i][j]);
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    answer = Math.max(answer, dp[i][j]);
+                }
             }
         }
-
         System.out.println(answer);
+
     }
 }
