eba8a81776ef1264f0f3d7e1b782f699aa7192e9
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2025-10-09 15:23:46 +0900

    [Gold V] Title: 풍선 맞추기, Time: 328 ms, Memory: 83480 KB -BaekjoonHub

--- 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.java
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.java
@@ -1,44 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static final int MAX = 1_000_000;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st=  new StringTokenizer(br.readLine());
+
+        int[] cnt = new int[MAX + 2];
 
         int n = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int index = 0; // 배열 인덱스
-        List<Integer> list = new ArrayList<>();
-        while(index < n){
-            if(list.size() == 0){
-                list.add(arr[index]);
-            }else{
-                boolean flag = false;
-                for (int i = 0; i < list.size(); i++) {
-                    if(list.get(i) - 1 == arr[index]){
-                        list.set(i, list.get(i) - 1);
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag) list.add(arr[index]);
+            int num = Integer.parseInt(st.nextToken());
+            if(cnt[num + 1] > 0){
+                cnt[num + 1] --;
             }
-
-            index ++;
+            cnt[num] ++;
         }
+        int answer = 0;
+        for(int item : cnt) answer += item;
 
-        System.out.println(list.size());
+        System.out.println(answer);
     }
 }
