3c7cc455d69a975086512b69b1d96799c621196c
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2026-01-05 21:04:32 +0900

    [Silver II] Title: 그르다 김가놈, Time: 508 ms, Memory: 88684 KB -BaekjoonHub

--- 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
@@ -1,60 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        // 김밥 꼬다리 자를거임
-        // 양쪽 균일 K만큼 잘라낼건데 2K보다 작으면 꼬다리 다자르고 나면 아무것도 없으니까 한쪽만 자름
-        // 꼬다리 잘라낸 김밥은 P로 다시 일정하게 자름 P로 자른 김밥 M개
-        // P를 최대한 길게 자르고 싶으면 ?
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(st.nextToken()); // 손질해야하는 김밥 개수
-        int k = Integer.parseInt(st.nextToken()); // 꼬다리 길이
-        int m = Integer.parseInt(st.nextToken()); // 김밥 최소개수
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int max = 0;
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-            max = Math.max(max, arr[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // n개를 꼬다리 k길이의 꼬다리만 잘라서 김밥 최소개수 보다 크게 어떻게 길이를 자르겠냐
+        for (int i = 0; i < n; i++) {
+            if(arr[i] >= 2 * k) arr[i] -= 2*k; // 양쪽다
+            else if(arr[i] <= 2 * k && arr[i] > k) arr[i] -= k; // 한쪽만
+            else if(arr[i] <= k) arr[i] = -1; // 폐기
+        }
+
         int left = 1;
         int right = 1_000_000_000;
 
-        int answer = -1;
+        int answer = 0;
         while(left <= right){
             int mid = (left + right) / 2;
-
-            int count = 0;
+            int sum = 0;
             for (int i = 0; i < n; i++) {
-                int num = arr[i];
-
-                // 꼬다리보다 작으면 폐기
-                if(num <= k) continue;
-
-                if(num < 2 * k) num -= k;
-                else{
-                    num -= 2 * k;
-                }
-
-                count += (num / mid);
+                if(arr[i] == -1) continue; // 폐기
+                sum += arr[i] / mid;
             }
 
-            if(count >= m){
-                answer = mid; // 가능한 후보
+            if(sum >= m){ // 자른개수가 최소개수보다 더많으면
+                answer = mid;
                 left = mid + 1;
             }else right = mid - 1;
-
         }
+        if(answer == 0){
+            System.out.println(-1);
+        }else System.out.println(answer);
 
-        System.out.println(answer);
     }
 }
