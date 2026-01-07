c76e206e78bd87b4a63608d074bf41ad39c90149
Seong-Jae Park <71877347+qkrtjdwo5662@users.noreply.github.com>
2026-01-08 00:12:19 +0900

    [Silver III] Title: 주유소, Time: 224 ms, Memory: 32076 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] distance = new int[n];
+        int[] value = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1 ; i++) {
+            distance[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            value[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 1; i++) {
+            if(value[i] < min) {
+                min = value[i];
+
+            }
+            answer += min * distance[i];
+        }
+        System.out.println(answer);
+
+    }
+}
